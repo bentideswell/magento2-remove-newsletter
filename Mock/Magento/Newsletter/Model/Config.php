9e2bf7647c32ebe8980ea7b801b355c78fb7c984
Ben Tideswell <ben@fishpig.co.uk>
2023-12-08 11:17:27 +0000

    1.0.2

---
+++ Mock/Magento/Newsletter/Model/Config.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ *
+ */
+declare(strict_types=1);
+
+namespace Magento\Newsletter\Model;
+
+class Config
+{
+    /**
+     *
+     */
+    public function isActive(
+        string $scopeType = ''
+    ): bool {
+        return false;
+    }
+}
+
